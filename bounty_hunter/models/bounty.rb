0f23c47eefa4e2892d0766370f56595cb74a0a98
Hamish <hamishfpso@gmail.com>
2017-10-10 18:22:11 +0100

    Implement Delete completed

--- bounty_hunter/models/bounty.rb
+++ bounty_hunter/models/bounty.rb
@@ -56,4 +56,17 @@ class Bounty
           db.close()
         end
 
+        def delete()
+          db = PG.connect({
+            dbname: 'bounty_hunter',
+            host: 'localhost'
+            })
+            sql = "DELETE FROM bounties
+            WHERE id = $1"
+            values = [@id]
+            db.prepare("delete_one", sql)
+            db.exec_prepared("delete_one", values)
+            db.close()
+          end
+
 end
